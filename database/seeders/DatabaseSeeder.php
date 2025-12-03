27b1c6cc79d3ee300d287e6ed020fb49ccb52697
fdurban11 <francis.durban@urios.edu.ph>
2025-12-03 11:51:05 +0800

    Add ProductSeeder with sample products for testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Seed sample products
+        $this->call(ProductSeeder::class);
     }
 }
